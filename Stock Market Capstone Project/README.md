309ed3c145e0e143828453dfa7b269626ebd1f42
akshit-agarwal-123 <akshit@email.arizona.edu>
2021-05-09 02:01:21 -0600

    docs: minor changes

--- Stock Market Capstone Project/README.md
+++ Stock Market Capstone Project/README.md
@@ -1,5 +1,12 @@
 # Stock market Capstone Project
 
+Analyse historic stock trends of Tesla, General Motors, Ford  Motor Co.
+  
+## Open Prices of [TSLA](https://www.tradingview.com/symbols/NASDAQ-TSLA/), [FORD](https://www.tradingview.com/symbols/NYSE-F/) and [GM](https://www.tradingview.com/symbols/NYSE-GM/)
+![open](/images/open%20prices.png)
+
+
+
 
 
 
--- Stock Market Capstone Project/stock market project.py
+++ Stock Market Capstone Project/stock market project.py
@@ -1,13 +1,26 @@
+from pandas import concat
 from pandas_datareader import get_data_yahoo
 
 # comparing stock performance of tesla, general motors and ford motor company
 
 # getting the data
 tsla_df = get_data_yahoo(symbols='tsla', start='2012-01-01', end='2017-01-01')
+gm_df = get_data_yahoo(symbols='gm', start='2012-01-01', end='2017-01-01')
+f_df = get_data_yahoo(symbols='f', start='2012-01-01', end='2017-01-01')
 print(tsla_df.head(100))
+print(gm_df.head(100))
+print(f_df.head(100))
+
+open_df = concat([tsla_df['Open'], gm_df['Open'], f_df['Open']], axis=1)
+# open_df['Date'] = to_datetime(open_df['Date'])
+# open_df = open_df.loc['2016-01-01':'2017-01-01', :]
+open_df.columns = ['TSLA', 'GM', 'FORD']
+
 
 # visualize the data - linear plot of Open prices
-tsla_df['Open'].plot(figsize=(16, 6))
+ax = open_df[['TSLA', 'GM', 'FORD']].plot(figsize=(16, 6))
+ax.set_xlabel("Trade Date")
+ax.set_ylabel("Open Prices")
 
 # 30 Day moving averages
 tsla_df['Open: 30 Day MA'] = tsla_df['Open'].rolling(30).mean()
