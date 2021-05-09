6b0902723d1a19e91fff8d59b7b50c640abed71e
akshit-agarwal-123 <akshit@email.arizona.edu>
2021-05-08 23:20:13 -0600

    feat: moving averages

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # Python-for-Financial-Analysis-and-Trading
 python, pandas, time series analysis, algorithmic trading, financial analysis, matplotlib, data visualization
+
+## 30-day Moving Average of Open prices of Walmart Stock
+![Moving](moving%20average%20plot.png)
--- time resampling.py
+++ time resampling.py
@@ -10,8 +10,30 @@ def main():
     # df['Date'] = to_datetime(df['Date'])
     # print(df.info())
 
-    print(df.resample(rule='A').mean())  # Year End Frequency
+    # mean, max, min
+    # print(df.resample(rule='A').mean())  # Year End Frequency
     print(df.resample(rule='M').mean())  # Month End Frequency
+
+    # df['Close'].resample(rule='M').mean().plot(kind='bar', figsize=(14, 4))
+
+    # ts.show()
+
+    print('')
+
+    print(df.shift(periods=-1).head())
+
+    print(df.tshift(freq='M').head())
+
+    # df['Open'].plot(figsize=(16, 6))
+
+    df['Open Moving Average'] = df['Open'].rolling(window=30).mean()
+    df[['Open', 'Open Moving Average']].plot(figsize=(16, 6))
+
+    print('')
+
+
+def first_day(entry):
+    return entry[0]
 
 
 if __name__ == '__main__':
